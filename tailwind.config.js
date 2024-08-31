624b9195ca2482309f6a1f6fb12971a44c0e1e8f
Tnixc <tnixxc@gmail.com>
2024-08-30 23:28:40 -0400

    new header + format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    darkMode: "selector",
-    theme: {
-        fontFamily: {
-            mono: ["basier_square_monoregular", "ui-monospace", "monospace", "menlo"],
-            sans: ["PretendardVariable", "system-ui", "ui-sans-serif"],
-            grotesk: ["OverusedGrotesk", "system-ui"]
-        },
-        extend: {},
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "selector",
+  theme: {
+    fontFamily: {
+      mono: ["basier_square_monoregular", "ui-monospace", "monospace", "menlo"],
+      sans: ["PretendardVariable", "system-ui", "ui-sans-serif"],
+      grotesk: ["OverusedGrotesk", "system-ui"],
     },
-    plugins: [],
+    extend: {},
+  },
+  plugins: [],
 };
