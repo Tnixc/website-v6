ad10caa408c117123faee8265790a241a45b945c
Tnixc <tnixxc@gmail.com>
2025-04-02 17:44:29 -0400

    now playing works

---
+++ dist/song.js
@@ -0,0 +1,20 @@
+async function updateSong() {
+  const res = await fetch("https://api.tnixc.space/api/current-song");
+  const data = await res.json();
+  const html = {
+    status: document.getElementById("song-status"),
+    cover: document.getElementById("song-cover-art"),
+    title: document.getElementById("song-title"),
+    artist: document.getElementById("song-artist")
+  }
+
+  html.cover.style.backgroundImage = `url(${data.image_url})`;
+
+  html.title.innerText = data.name;
+  html.artist.innerHTML = data.artist;
+  html.status.setAttribute("data-playing", data.now_playing)
+}
+
+updateSong();
+
+setInterval(updateSong, 1000);
