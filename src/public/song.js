8f7b98472e81ebed5eae7ca06dec816e4f85f285
Tnixc <tnixxc@gmail.com>
2025-11-09 12:34:03 -0500

    music

--- src/public/song.js
+++ src/public/song.js
@@ -6,13 +6,24 @@ async function updateSong() {
     cover: document.getElementById("song-cover-art"),
     title: document.getElementById("song-title"),
     artist: document.getElementById("song-artist"),
+    songInfo: document.getElementById("song-info"),
+    notListening: document.getElementById("not-listening"),
   };
 
-  html.cover.style.backgroundImage = `url(${data.image_url})`;
-
-  html.title.innerText = data.name;
-  html.artist.innerHTML = data.artist;
   html.status.setAttribute("data-playing", data.now_playing);
+
+  if (data.now_playing) {
+    html.cover.style.backgroundImage = `url(${data.image_url})`;
+    html.title.innerText = data.name;
+    html.artist.innerHTML = data.artist;
+    html.cover.classList.remove("hidden");
+    html.songInfo.classList.remove("hidden");
+    html.notListening.classList.add("hidden");
+  } else {
+    html.cover.classList.add("hidden");
+    html.songInfo.classList.add("hidden");
+    html.notListening.classList.remove("hidden");
+  }
 }
 
 updateSong();
